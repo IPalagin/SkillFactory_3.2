093a6b34ddbf3541d09790b86d66c61802c63c27
IPalagin <igor_palagin.56rus@mail.ru>
2023-04-03 22:02:22 +0300

    Изучение переменных

--- SkillFactory_3.2/Program.cs
+++ SkillFactory_3.2/Program.cs
@@ -4,13 +4,15 @@
     {
         public static void Main(string[] args)
         {
+            string MyName = "Igor";
+            byte MyAge = 29;
+            bool HaveIApet = true;
+            double MyShoeSize = 45;
 
-            string myName;
-            myName = "Igor";
-
-            Console.WriteLine(myName);
-
-            Console.ReadKey();
+            Console.WriteLine("Меня зовут " + MyName);
+            Console.WriteLine("Мне " + MyAge);
+            Console.WriteLine("Есть ли у меня питомец ? " + HaveIApet);
+            Console.WriteLine("Мой размер ноги " + MyShoeSize);
         }
     }
 }
