85f369de6bedebc61b2bd82241c79afbb85ea71a
IPalagin <igor_palagin.56rus@mail.ru>
2023-04-03 21:31:41 +0300

    Добавьте файлы проекта.

---
+++ SkillFactory_3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SkillFactory_3._2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string myName;
+            myName = "Igor";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
